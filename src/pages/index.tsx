49205ec87f4b6ad8f71f92722705c7b922602904
elzup <guild0105@gmail.com>
2020-01-09 10:00:05 +0900

    fix: link warnings

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -35,7 +35,7 @@ function BlogIndex({
                 marginBottom: rhythm(1 / 4),
               }}
             >
-              <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
+              <Link style={{ boxShadow: `none` }} to={`/${node.fields.slug}`}>
                 {title}
               </Link>
             </h3>
--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -54,14 +54,14 @@ function BlogPostTemplate(props: Props) {
       >
         <li>
           {previous && (
-            <Link to={previous.fields.slug} rel="prev">
+            <Link to={'/' + previous.fields.slug} rel="prev">
               ← {previous.frontmatter.title}
             </Link>
           )}
         </li>
         <li>
           {next && (
-            <Link to={next.fields.slug} rel="next">
+            <Link to={'/' + next.fields.slug} rel="next">
               {next.frontmatter.title} →
             </Link>
           )}
