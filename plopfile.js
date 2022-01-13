770a31cdb5f573a6644ecf6e7037d2760ebf93ba
elzup <guild0105@gmail.com>
2022-01-13 22:04:18 +0900

    add slug validation on plop

--- plopfile.js
+++ plopfile.js
@@ -1,6 +1,6 @@
 /* eslint-disable-next-line import/no-extraneous-dependencies */
 
-const pad00 = num => String(num).padStart(2, '0')
+const pad00 = (num) => String(num).padStart(2, '0')
 const date = new Date()
 const year = date.getFullYear()
 const month = pad00(date.getMonth() + 1)
@@ -8,11 +8,11 @@ const day = pad00(date.getDate())
 const hms = `${pad00(date.getHours())}:00:00`
 const datePrefix = `${year}-${month}-${day}`
 
-module.exports = function(
+module.exports = function (
   /** @type {import('plop').NodePlopAPI} */
   plop
 ) {
-  plop.setHelper('getKebabFirst', date => date.split('-')[0])
+  plop.setHelper('getKebabFirst', (date) => date.split('-')[0])
   plop.setHelper('hms', () => hms)
   plop.setGenerator('post', {
     description: 'Write new blog post',
@@ -21,8 +21,24 @@ module.exports = function(
       {
         type: 'input',
         name: 'id',
-        message: `id(kebab-case):`,
-        validate: Boolean,
+        message: `id(kebab-case, [a-z0-9_-], 12...20):`,
+        validate: function (input) {
+          // Declare function as asynchronous, and save the done callback
+          const done = this.async()
+          if (!input) {
+            done('required')
+            return
+          }
+          if (!/^[a-z0-9_-]+$/.test(input)) {
+            done('available chars [a-z0-9_-]')
+            return
+          }
+          if (input.length < 12 || 20 < input.length) {
+            done('length 12...20')
+            return
+          }
+          done(null, true)
+        },
       },
       {
         type: 'input',
